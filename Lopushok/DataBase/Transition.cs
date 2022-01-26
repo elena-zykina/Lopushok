f8c0676eb78c454771476877284ef546ba3349c5
Елена Зыкина <lenazykina017@gmail.com>
2022-01-26 13:16:37 +0300

    Добавьте файлы проекта.

---
+++ Lopushok/DataBase/Transition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lopushok.DataBase
+{
+    class Transition
+    {
+        public static Frame MainFrame { get; set; }
+
+        private static LopushokEntities context;
+        public static LopushokEntities Context
+        {
+            get
+            {
+                if (context == null)
+                    context = new LopushokEntities();
+
+                return context;
+            }
+        }
+    }
+}
---
+++ Lopushok/Pages/ProductList.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Lopushok.DataBase;
+
+namespace Lopushok.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductList.xaml
+    /// </summary>
+    public partial class ProductList : Page
+    {
+        public ProductList()
+        {
+            InitializeComponent();
+            var filtItems = Transition.Context.ProductType.ToList();
+            filtItems.Insert(0, new ProductType { Title = "Все элементы" });
+            FiltCBox.ItemsSource = filtItems;
+
+            FiltCBox.SelectedIndex = 0;
+            SortCBox.SelectedIndex = 0;
+
+            ListProduct.ItemsSource = Transition.Context.Product.ToList();
+        }
+
+        private void DataView()
+        {
+            var tempDataProduct = Transition.Context.Product.ToList();
+
+            if (FiltCBox.SelectedIndex > 0)
+                tempDataProduct = tempDataProduct
+                    .Where(p => p.ProductTypeID == (FiltCBox.SelectedItem as ProductType).ID)
+                    .ToList();
+
+            if (SearchProductBox.Text != "Введите для поиска" &&
+                SearchProductBox.Text != null)
+                tempDataProduct = tempDataProduct
+                    .Where(p => p.Title.ToLower().Contains(SearchProductBox.Text.ToLower())
+                    || p.Description.ToLower().Contains(SearchProductBox.Text.ToLower()))
+                    .ToList();
+
+            switch (SortCBox.SelectedIndex)
+            {
+                case 1:
+                    {
+                        if (!(bool)CheckSort.IsChecked)
+                            tempDataProduct = tempDataProduct
+                                    .OrderBy(p => p.Title)
+                                    .ToList();
+                        else
+                            tempDataProduct = tempDataProduct
+                                    .OrderByDescending(p => p.Title)
+                                    .ToList();
+
+                        ListProduct.ItemsSource = tempDataProduct;
+                        break;
+                    }
+                case 2:
+                    {
+                        if (!(bool)CheckSort.IsChecked)
+                            tempDataProduct = tempDataProduct
+                                    .OrderBy(p => p.ProductionWorkshopNumber)
+                                    .ToList();
+                        else
+                            tempDataProduct = tempDataProduct
+                                    .OrderByDescending(p => p.ProductionWorkshopNumber)
+                                    .ToList();
+
+                        ListProduct.ItemsSource = tempDataProduct;
+                        break;
+                    }
+                case 3:
+                    {
+                        if (!(bool)CheckSort.IsChecked)
+                            tempDataProduct = tempDataProduct
+                                    .OrderBy(p => p.MinCostForAgent)
+                                    .ToList();
+                        else
+                            tempDataProduct = tempDataProduct
+                                    .OrderByDescending(p => p.MinCostForAgent)
+                                    .ToList();
+
+                        ListProduct.ItemsSource = tempDataProduct;
+                        break;
+                    }
+            }
+            ListProduct.ItemsSource = tempDataProduct;
+
+            CountPages.Text = $"Страница: {"1"} из {CountPages}";
+        }
+
+        private void SearchProductBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SearchProductBox.Text != "Введите для поиска")
+                DataView();
+        }
+
+        private void SearchProductBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(SearchProductBox.Text))
+                SearchProductBox.Text = "Введите для поиска";
+        }
+
+        private void SearchProductBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SearchProductBox.Text = null;
+        }
+
+        private void SortCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataView();
+        }
+
+        private void CheckSort_Unchecked(object sender, RoutedEventArgs e)
+        {
+            DataView();
+        }
+
+        private void CheckSort_Checked(object sender, RoutedEventArgs e)
+        {
+            DataView();
+        }
+
+        private void FiltCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataView();
+        }
+
+        private void ProductsView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteProdBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProductsView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListProduct.SelectedItems.Count > 0)
+                DeleteProdBtn.Visibility = Visibility.Visible;
+            else
+                DeleteProdBtn.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
